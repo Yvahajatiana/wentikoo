45b7a1d4c88b2a5c0a3e78f15bc64a8530922399
Yvahajatiana <yvahajatiana@gmail.com>
2019-10-02 23:31:32 +0200

    Add project files.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Hashdji.Software.Core.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class EntityBase
+    {
+        public long Id { get; set; }
+
+        public Guid UniqueId { get; set; }
+    }
+}
---
+++ Core/Entities/Offer.cs
@@ -0,0 +1,23 @@
+﻿namespace Hashdji.Software.Core.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Offer : EntityBase
+    {
+        public DateTime DepartureDate { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+        public float TotalWeight { get; set; }
+
+        public float Price { get; set; }
+
+        public string TransportType { get; set; }
+
+        public string 
+    }
+}
